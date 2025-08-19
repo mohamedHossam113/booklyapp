14661195b3b9d79316dd64beb98eb7ffcf2752dc
mohamedHossam113 <rues185@gmail.com>
2025-08-19 10:57:07 +0300

    Add splash screen components and update main app structure
    - Introduced SplashView and SplashViewBody widgets for the splash screen.
    - Added logo and text assets for the splash screen.
    - Updated main application entry point to use GetMaterialApp and set SplashView as the home.
    - Included new assets for the logo and test image.
    - Removed unused widget test file.

---
+++ lib/Features/splash/views/widgets/splash_view_body.dart
@@ -0,0 +1,23 @@
+import 'package:booklyapp/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+
+class SplashViewBody extends StatelessWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset(
+          AssetsData.logo,
+        ),
+        const Text(
+          'Your Gate To E-Books..',
+          textAlign: TextAlign.center,
+        )
+      ],
+    );
+  }
+}
---
+++ lib/Features/splash/views/widgets/splashview.dart
@@ -0,0 +1,19 @@
+import 'package:booklyapp/Features/splash/views/widgets/splash_view_body.dart';
+import 'package:flutter/material.dart';
+
+class SplashView extends StatefulWidget {
+  const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color(0xff100B20),
+      body: SplashViewBody(),
+    );
+  }
+}
