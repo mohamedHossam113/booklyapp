d4c230d635986a0447a1b45464dda72031bd0d38
mohamedHossam113 <rues185@gmail.com>
2025-08-21 11:20:31 +0300

    Add CustomListViewItem component and integrate it into HomeViewBody

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:booklyapp/Features/home/presentation/views/widgets/custom_appbar.dart';
+import 'package:booklyapp/Features/home/presentation/views/widgets/custom_list_view.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -9,7 +10,23 @@ class HomeViewBody extends StatelessWidget {
     return const Column(
       children: [
         CustomAppBar(),
+        CustomListViewItem(),
+        Expanded(child: FeaturedBooksList())
       ],
+    );
+  }
+}
+
+class FeaturedBooksList extends StatelessWidget {
+  const FeaturedBooksList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      scrollDirection: Axis.horizontal,
+      itemBuilder: (context, index) {
+        return const CustomListViewItem();
+      },
     );
   }
 }
